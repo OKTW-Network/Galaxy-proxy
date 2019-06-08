3b37c82282f9c6b83116efa9f0dfa7e699dac2a6
james58899 <james59988@gmail.com>
2019-06-08 12:50:19 +0800

    Maven

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,20 +1,9 @@
-import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-buildscript {
-    val kotlinVersion = "1.3.21"
-    repositories { jcenter() }
-
-    dependencies {
-        classpath("org.jetbrains.kotlin", "kotlin-serialization", kotlinVersion)
-    }
-}
-
 plugins {
+    `maven-publish`
     kotlin("jvm")
-    id("com.github.johnrengelman.shadow")
 }
-apply(plugin = "kotlinx-serialization")
 
 group = "one.oktw"
 version = "1.0-SNAPSHOT"
@@ -27,22 +16,16 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.mongodb", "bson", "3.10.1")
-    shadow(kotlin("stdlib-jdk8"))
-    shadow("org.mongodb", "bson", "3.10.1")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-val shadowJar by tasks.getting(ShadowJar::class) {
-    classifier = ""
-    configurations = listOf(project.configurations.shadow.get())
-    exclude("META-INF")
-    minimize()
-}
-
-tasks.getByName<Jar>("jar") {
-    dependsOn(shadowJar)
-    enabled = false
+publishing {
+    publications {
+        create<MavenPublication>("api") {
+            from(components["java"])
+        }
+    }
 }
