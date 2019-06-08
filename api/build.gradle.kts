d7a6b0aa3373ab732fae24cfe30d85250e9cc482
james58899 <james59988@gmail.com>
2019-06-08 13:57:17 +0800

    Add flat jar

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,8 +1,10 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     `maven-publish`
     kotlin("jvm")
+    id("com.github.johnrengelman.shadow")
 }
 
 group = "one.oktw"
@@ -16,16 +18,31 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("org.mongodb", "bson", "3.10.1")
+
+    shadow(kotlin("stdlib-jdk8"))
+    shadow("org.mongodb", "bson", "3.10.1")
 }
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
+val shadowJar by tasks.getting(ShadowJar::class) {
+    classifier = "all"
+    configurations = listOf(project.configurations.shadow.get())
+    exclude("META-INF")
+    minimize()
+}
+
+tasks.getByName<Jar>("jar") {
+    dependsOn(shadowJar)
+}
+
 publishing {
     publications {
         create<MavenPublication>("api") {
             from(components["java"])
+            artifact(shadowJar)
         }
     }
 }
