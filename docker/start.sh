ffc43dd59c403a98aa1d2d6b485abd618d13333b
James58899 <james59988@gmail.com>
2021-05-17 11:22:04 +0800

    Yes i-understand-what-im-doing.

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
+#!/usr/bin/env ash
 # Hack configmap
 [[ -e config/velocity.toml ]] && cp -f config/velocity.toml velocity.toml
 
 # Start
-exec java -jar velocity.jar
+exec java -Dvelocity.i-understand-what-im-doing=true -jar velocity.jar
