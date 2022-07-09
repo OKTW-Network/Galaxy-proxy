5574ed03eb570c49450c826d0276b3bbbc695365
jimchen5209 <jimchen5209@gmail.com>
2022-07-09 09:21:47 +0800

    Rename variable

--- src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
@@ -6,10 +6,10 @@ import one.oktw.galaxy.proxy.Main.Companion.main
 class ResourcePackHelper {
     companion object{
         fun trySendResourcePack(player: Player, galaxy: String){
-            val lobbyResourcePack = main.config.galaxiesResourpacePack[galaxy] ?: return
+            val resourcePack = main.config.galaxiesResourpacePack[galaxy] ?: return
             player.sendResourcePackOffer(
-                main.proxy.createResourcePackBuilder(lobbyResourcePack.uri.toString())
-                    .setHash(lobbyResourcePack.hash)
+                main.proxy.createResourcePackBuilder(resourcePack.uri.toString())
+                    .setHash(resourcePack.hash)
                     .setShouldForce(true)
                     .build()
             )
