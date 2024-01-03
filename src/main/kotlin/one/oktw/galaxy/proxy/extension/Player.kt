fbeadee328073b2ad42b5513361fc4faee2036b3
James58899 <james59988@gmail.com>
2024-01-03 11:39:50 +0800

    Try remove old resource pack

--- src/main/kotlin/one/oktw/galaxy/proxy/extension/Player.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/extension/Player.kt
@@ -1,6 +1,13 @@
 package one.oktw.galaxy.proxy.extension
 
 import com.velocitypowered.api.proxy.Player
-import com.velocitypowered.api.proxy.server.ServerPing
+import one.oktw.galaxy.proxy.Main.Companion.main
 
-fun Player.toSamplePlayer() = ServerPing.SamplePlayer(username, uniqueId)
+fun Player.sendPacket(o: Any) {
+    try {
+        val connection = this.javaClass.getMethod("getConnection").invoke(this)
+        connection.javaClass.getMethod("write", Any::class.java).invoke(connection, o)
+    } catch (e: Exception) {
+        main.logger.error("Call velocity method error.", e)
+    }
+}
