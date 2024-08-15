49908b7c9639b5aca8c8033c6ca95c62080b7250
jimchen5209 <jimchen5209@gmail.com>
2024-08-15 14:06:38 +0800

    fix: resourcePack can't be reapply after RemoveResourcePackPacket - use clearResourcePacks instead

--- src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/resourcepack/ResourcePackHelper.kt
@@ -2,19 +2,13 @@ package one.oktw.galaxy.proxy.resourcepack
 
 import com.velocitypowered.api.proxy.Player
 import one.oktw.galaxy.proxy.Main.Companion.main
-import one.oktw.galaxy.proxy.extension.sendPacket
 
 class ResourcePackHelper {
     companion object{
         fun trySendResourcePack(player: Player, galaxy: String){
             val resourcePack = main.config.galaxiesResourcePack[galaxy] ?: return
-            // workaround Minecraft 1.20.3 not auto unload old resource pack
-            try {
-                val packet = Class.forName("com.velocitypowered.proxy.protocol.packet.RemoveResourcePackPacket").getConstructor().newInstance()
-                player.sendPacket(packet)
-            } catch (e: Exception) {
-                main.logger.error("Send remove resource pack packet error.", e)
-            }
+
+            player.clearResourcePacks()
             player.sendResourcePackOffer(
                 main.proxy.createResourcePackBuilder(resourcePack.uri.toString())
                     .setHash(resourcePack.hash)
