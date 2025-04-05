fc596c04c063a16b31178974708f8cce74437e93
jimchen5209 <jimchen5209@gmail.com>
2025-04-05 16:02:48 +0800

    feat: bump minecraft version 1.21.5

--- src/main/kotlin/one/oktw/galaxy/proxy/config/model/ProxyConfig.kt
+++ src/main/kotlin/one/oktw/galaxy/proxy/config/model/ProxyConfig.kt
@@ -1,5 +1,5 @@
 package one.oktw.galaxy.proxy.config.model
 
 data class ProxyConfig(
-    val ProtocolVersion: Int = 768 // default Minecraft 1.21.3
+    val ProtocolVersion: Int = 770 // default Minecraft 1.21.5
 )
